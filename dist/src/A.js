a68c393fd9933bb354b085518cdfaa645777d2be
axtk <axtkx@inbox.ru>
2021-07-13 00:55:20 +0300

    updated link click handler

--- dist/src/A.js
+++ dist/src/A.js
@@ -3,13 +3,17 @@ import { hasRouteLinkProps, getPath } from '@axtk/router';
 import { RouteContext } from './RouteContext';
 export const A = ({ href, target, onClick, ...otherProps }) => {
     let route = useContext(RouteContext);
-    let onLinkClick = useCallback(event => {
-        if (hasRouteLinkProps({ href, target })) {
+    let onLinkClick = useCallback((event) => {
+        if (onClick)
+            onClick(event);
+        if (event.defaultPrevented)
+            return;
+        let hasModifierKey = event.ctrlKey || event.shiftKey || event.altKey || event.metaKey;
+        let isDefaultClick = event.button === 0 && !hasModifierKey;
+        if (isDefaultClick && hasRouteLinkProps({ href, target })) {
             event.preventDefault();
             route.assign(getPath(href));
         }
-        if (onClick)
-            onClick(event);
     }, [route, href, target, onClick]);
     return createElement('a', { href, target, onClick: onLinkClick, ...otherProps });
 };
--- src/A.ts
+++ src/A.ts
@@ -1,17 +1,24 @@
-import {useContext, useCallback, createElement, AnchorHTMLAttributes} from 'react';
+import {useContext, useCallback, createElement, AnchorHTMLAttributes, MouseEvent} from 'react';
 import {hasRouteLinkProps, getPath} from '@axtk/router';
 import {RouteContext} from './RouteContext';
 
 export const A = ({href, target, onClick, ...otherProps}: AnchorHTMLAttributes<HTMLAnchorElement>) => {
     let route = useContext(RouteContext);
 
-    let onLinkClick = useCallback(event => {
-        if (hasRouteLinkProps({href, target})) {
+    let onLinkClick = useCallback((event: MouseEvent<HTMLAnchorElement>) => {
+        if (onClick)
+            onClick(event);
+
+        if (event.defaultPrevented)
+            return;
+
+        let hasModifierKey = event.ctrlKey || event.shiftKey || event.altKey || event.metaKey;
+        let isDefaultClick = event.button === 0 && !hasModifierKey;
+
+        if (isDefaultClick && hasRouteLinkProps({href, target})) {
             event.preventDefault();
             route.assign(getPath(href));
         }
-
-        if (onClick) onClick(event);
     }, [route, href, target, onClick]);
 
     return createElement('a', {href, target, onClick: onLinkClick, ...otherProps});
